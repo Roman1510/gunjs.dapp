a336d432b3e251504c96cb0ae8624610185b0e59
Roman1510 <romanvinnick@gmail.com>
2022-01-27 23:34:03 +0100

    added avatar api

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,10 @@
   <header>
     <h1></h1>
     <div v-if="username" class="user-bio">
-      <span>Hello <strong>Username</strong></span>
-      <img src="" alt="avatar" />
+      <span
+        >Hello <strong>{{ username }}</strong></span
+      >
+      <img :src="getAvatarURL" alt="avatar" />
       <button @click.prevent="signout" class="signout-button">Sign Out</button>
     </div>
     <h3>GUN-DAPP</h3>
@@ -12,9 +14,13 @@
 <script>
 import { user, username } from '../util/User'
 import { useRouter } from 'vue-router'
+import { computed } from 'vue'
 export default {
   name: 'Header',
   setup() {
+    const getAvatarURL = computed(() => {
+      return `https://avatars.dicebear.com/api/identicon/${username.value}.svg`
+    })
     const router = useRouter()
     const signout = () => {
       user.leave()
@@ -23,7 +29,8 @@ export default {
     }
     return {
       signout,
-      username
+      username,
+      getAvatarURL
     }
   }
 }
